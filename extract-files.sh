c3ec2b8cc726d2eb48710c3d9badcc617dd610a2
Alexander Winkowski <dereference23@outlook.com>
2024-05-08 03:03:55 +0530

    Revert "miatoll: nfc: Update storage path to /data/vendor/nfc"
    This reverts commit 2d6f1e59435e38046e5cdf7e346fde18ed9b1461.
    
    No denials were observed without this change but there are denials
    with it. Probably the original issue was fixed in newer SELinux policy.
    
    Change-Id: Ifc41ea29174d5752bd2f96a3248b6ff96ffb9e9f

--- extract-files.sh
+++ extract-files.sh
@@ -62,9 +62,6 @@ function blob_fixup() {
         vendor/etc/init/android.hardware.keymaster@4.0-service-qti.rc)
             sed -i "s/4\.0/4\.1/g" "${2}"
             ;;
-        vendor/etc/libnfc-nci.conf)
-            sed -i "s/\/data\/nfc/\/data\/vendor\/nfc/g" "${2}"
-            ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so)
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
