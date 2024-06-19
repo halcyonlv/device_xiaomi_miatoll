b9a4b47f53250173ba643d9168b20cf2e083fb8c
klozz <klozz@thexperienceproject.org>
2024-06-19 15:26:24 +0530

    miatoll: Build android.hidl.[manager,memory.block]@1.0 and their vendor variant
    Needed by misys:
    CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.xiaomi.hardware.misys@3.0-
    service": library "android.hidl.memory.block@1.0.so" not found: needed by
     /vendor/lib64/vendor.xiaomi.hardware.misys@3.0.so in namespace (default)

--- miatoll.mk
+++ miatoll.mk
@@ -168,6 +168,12 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
