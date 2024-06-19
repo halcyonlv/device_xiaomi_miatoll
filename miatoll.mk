fbef2b8a92d8b1470ad1a11783fb20f50ec9b26a
Volodymyr Zhdanov <wight554@gmail.com>
2024-06-19 15:19:26 +0530

    miatoll: build libhidltransport and libhwbinder

--- miatoll.mk
+++ miatoll.mk
@@ -163,6 +163,9 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
