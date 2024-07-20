d42df94dac818c4051e66fea76e96a3872b8bd4c
Bruno Martins <bgcngm@gmail.com>
2024-07-21 03:18:08 +0530

    miatoll: Convert WiFi firmware symlinks to install_symlink targets The last bit that was preventing building hermetic images.
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- miatoll.mk
+++ miatoll.mk
@@ -491,5 +491,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
