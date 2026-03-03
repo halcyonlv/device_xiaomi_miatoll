008d58badecd806e03fac1c16a60535e436caa7c
herobuxx <me@herobuxx.me>
2026-03-03 14:26:43 +0000

    miatoll: hlcyn: Enchanting Bloom
    Change-Id: I1d189bd5c6e4c9579d5f887ebecfe70d130953f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/halcyon/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 
